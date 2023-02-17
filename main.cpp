30ae5e0bf9a5a15e87d558c7d94a1f30f5d0c39c
Ilya Tsoy <inqlest@gmail.com>
2023-02-18 01:03:53 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define SUB(a,b) ((a)-(b))
+#include <iostream>
+
+int main ()
+{
+	int a = 7;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
